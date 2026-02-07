259e9ce6fceabf8c56f08bf7ee1e4eecb73f82ca
Miguel Ortiz <maortizp@eafit.edu.co>
2026-02-07 18:03:14 -0500

    taller resuelto

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import movie
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Miguel Angel Ortiz Puerta'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+def about(request):
+    return HttpResponse('This is the about page')
